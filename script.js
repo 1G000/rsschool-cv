6692f05d5795d8df8baa076ff7c11a18faa744bd
User <gorbatovskiy.igor@gmail.com>
2023-10-25 21:52:29 +0300

    feat: add burger menu and refactor styles

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const leftBtn = document.querySelector(".left-btn");
 const rightBtn = document.querySelector(".right-btn");
-const slider = document.querySelector(".cert");
+const slider = document.querySelector(".projects");
 let slideQty = 0;
+const burgerMenuBtn = document.querySelector(".burger-menu-icon");
+const line = document.querySelector(".line");
+const burgerMenu = document.querySelector(".burger-menu");
+
+burgerMenuBtn.addEventListener("click", () => {
+  burgerMenuBtn.classList.toggle("active");
+  burgerMenu.classList.toggle("burger-menu-open");
+});
+
 function moveLeft() {
   if (slideQty === 0) {
     slideQty = 800;
