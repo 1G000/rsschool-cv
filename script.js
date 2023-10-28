14e0cbdf01f09e0d70e61318d4c741536389aea3
User <gorbatovskiy.igor@gmail.com>
2023-10-28 12:15:32 +0300

    feat: add backdrop function on burger-menu

--- script.js
+++ script.js
@@ -5,11 +5,22 @@ let slideQty = 0;
 const burgerMenuBtn = document.querySelector(".burger-menu-icon");
 const line = document.querySelector(".line");
 const burgerMenu = document.querySelector(".burger-menu");
+const backDrop = document.querySelector(".backdrop");
+const links = document.querySelectorAll(".nav-link-burger");
 
 burgerMenuBtn.addEventListener("click", () => {
   burgerMenuBtn.classList.toggle("active");
   burgerMenu.classList.toggle("burger-menu-open");
+  backDrop.classList.toggle("is-hidden");
 });
+
+function toggleBackDrop() {
+  backDrop.classList.toggle("is-hidden");
+  burgerMenuBtn.classList.toggle("active");
+  burgerMenu.classList.toggle("burger-menu-open");
+}
+
+links.forEach((link) => link.addEventListener("click", toggleBackDrop));
 
 function moveLeft() {
   if (slideQty === 0) {
