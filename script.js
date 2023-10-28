7b1acb9c4e6a5b600daeb83392f40ee83b0dcc68
User <gorbatovskiy.igor@gmail.com>
2023-10-28 14:43:22 +0300

    fix: slider, burger and some styles

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const line = document.querySelector(".line");
 const burgerMenu = document.querySelector(".burger-menu");
 const backDrop = document.querySelector(".backdrop");
 const links = document.querySelectorAll(".nav-link-burger");
+const itemsList = document.querySelector(".items-list");
+
+window.addEventListener("resize", () => {
+  burgerMenuBtn.classList.remove("active");
+  burgerMenu.classList.remove("burger-menu-open");
+  backDrop.classList.add("is-hidden");
+});
 
 burgerMenuBtn.addEventListener("click", () => {
   burgerMenuBtn.classList.toggle("active");
@@ -20,24 +27,32 @@ function toggleBackDrop() {
   burgerMenu.classList.toggle("burger-menu-open");
 }
 
-links.forEach((link) => link.addEventListener("click", toggleBackDrop));
+window.addEventListener("mouseup", function (event) {
+  if (
+    burgerMenu.classList.contains("burger-menu-open") &&
+    event.target != burgerMenuBtn &&
+    event.target != burgerMenu
+  ) {
+    toggleBackDrop();
+  }
+});
 
 function moveLeft() {
-  if (slideQty === 0) {
-    slideQty = 800;
+  if (slideQty <= 0) {
+    slideQty = 1000;
     slider.style.marginLeft = -slideQty + "px";
   } else {
-    slideQty -= 400;
+    slideQty -= 380;
     slider.style.marginLeft = -slideQty + "px";
   }
 }
 
 function moveRight() {
-  if (slideQty === 800) {
+  if (slideQty >= 900) {
     slideQty = 0;
     slider.style.marginLeft = -slideQty + "px";
   } else {
-    slideQty += 400;
+    slideQty += 380;
     slider.style.marginLeft = -slideQty + "px";
   }
 }
